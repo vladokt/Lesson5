af30900e3f729a56516cf2a691dd86c5331ec675
vladokt <vlad78tomsk@mail.ru>
2022-05-10 19:36:20 +0700

    Ok

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Lesson5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("Operations with vectors.");
+            Console.ReadLine();
+
+            Vector vector1 = new Vector(1, -2, 3);
+            Vector vector2 = new Vector(-4, 5, 6);
+
+            Console.WriteLine("Vector 1: " + vector1.ToString());
+            Console.WriteLine("Vector 2: " + vector2.ToString());
+            Console.ReadLine();
+
+            Console.WriteLine($"Vector 1 modulus: " + vector1.Mod().ToString("f5"));
+            Console.WriteLine($"Vector 2 modulus: " + vector2.Mod().ToString("f5"));
+            Console.ReadLine();
+
+            Console.WriteLine($"Scalar product of vectors: {Vector.ScalarMul(vector1, vector2)}");
+            Console.ReadLine();
+
+            Console.WriteLine("Vector product of vectors: " + (vector1 * vector2).ToString());
+            Console.ReadLine();
+
+            Console.WriteLine($"Cosine of the angle between vectors: " + Vector.CosAngle(vector1, vector2).ToString("f8"));
+            Console.ReadLine();
+
+            Console.WriteLine("Sum of vectors: " + (vector1 + vector2).ToString());
+            Console.ReadLine();
+
+            Console.WriteLine("Vector difference: " + (vector1 - vector2).ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson5/Vector.cs
@@ -0,0 +1,47 @@
+﻿namespace Lesson5
+{
+    public class Vector
+    {
+        public int x, y, z;
+        public Vector(int xcoord, int ycoord, int zcoord)
+        {
+            x = xcoord;
+            y = ycoord;
+            z = zcoord;
+        }
+
+        public override string ToString()
+        {
+            return $"(x = {x}, y = {y}, z = {z})";
+        }
+        public double Mod()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public static double ScalarMul(Vector v1,Vector v2)
+        {
+            return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+        }
+
+        public static Vector operator * (Vector v1, Vector v2)
+        {
+            return new Vector(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
+        }
+
+        public static double CosAngle(Vector v1, Vector v2)
+        {
+            return Vector.ScalarMul(v1, v2) / (v1.Mod() * v2.Mod());
+        }
+
+        public static Vector operator + (Vector v1, Vector v2)
+        {
+            return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+        }
+
+        public static Vector operator - (Vector v1, Vector v2)
+        {
+            return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        }
+    }
+}
